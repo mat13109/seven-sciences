08eab9d95f0979013011fc88ca9487e26b4091b0
mat13109 <mathis-abdou@outlook.com>
2026-04-20 17:18:00 +0200

    Removed unused code

--- Scripts/Calculus/UpgradeGroup.cs
+++ Scripts/Calculus/UpgradeGroup.cs
@@ -4,21 +4,16 @@ namespace SevenScience;
 
 public partial class UpgradeGroup : CheckButton
 {
-    private const int BaseGroupValue = 7;
-    private const int UpgradedGroupValue = 10;
+	private const int BaseGroupValue = 7;
+	private const int UpgradedGroupValue = 10;
 
-    private CheckButton _upgradeGroupButton;
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() => _upgradeGroupButton = GetNode<CheckButton>(".");
-
-    /// <summary>
+	/// <summary>
 	/// Trigger when the check button is pressed
 	/// </summary>
 	/// <param name="isToggle">If the group upgrade leader's card is active</param>
 	private static void _on_toggled(bool isToggle)
-    {
-        Maths.GroupValue = isToggle ? UpgradedGroupValue : BaseGroupValue;
-        Main.OnCardCountChanged?.Invoke();
-    }
+	{
+		Maths.GroupValue = isToggle ? UpgradedGroupValue : BaseGroupValue;
+		Main.OnCardCountChanged?.Invoke();
+	}
 }
